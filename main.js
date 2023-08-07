6d47370a67d230557849668c306a2293fe628352
thanigai1709 <babutr59@gmail.com>
2023-08-07 21:07:29 +0530

    lenis smooth scroll added & text captions added

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const context = canvas.getContext("2d");
 const frameCount = 521;
 const img = new Image();
 const setting = document.querySelector(".setting button");
-
+let isAutoPlaying = false;
 const images = [];
 const airpods = {
 	frame: 0,
@@ -36,17 +36,90 @@ const scene1 = gsap.timeline({
 	},
 });
 
-scene1.to(
-	bannerTexts,
-	{
-		opacity: 0,
-		x: "-100%",
-		duration: 1.5,
-		ease: "expo.inOut",
-		stagger: 0.05,
-	},
-	"-=10"
-);
+scene1
+	.to(
+		bannerTexts,
+		{
+			opacity: 0,
+			x: "-100%",
+			duration: 0.8,
+			stagger: 0.05,
+			ease: "expo.inOut",
+		},
+		"-=10"
+	)
+	.to(
+		".animation-captions__item.caption--1",
+		{
+			opacity: 1,
+			duration: 0.6,
+			ease: "power3.inOut",
+		},
+		"-=9.9"
+	)
+	.to(
+		".animation-captions__item.caption--1",
+		{
+			opacity: 0,
+			duration: 0.4,
+			ease: "power3.inOut",
+		},
+		"-=9"
+	)
+	.to(
+		".animation-captions__item.caption--2",
+		{
+			opacity: 1,
+			duration: 0.6,
+			ease: "expo.inOut",
+		},
+		"-=8.9"
+	)
+	.to(
+		".animation-captions__item.caption--2",
+		{
+			opacity: 0,
+			duration: 0.4,
+			ease: "expo.inOut",
+		},
+		"-=8"
+	)
+	.to(
+		".animation-captions__item.caption--3",
+		{
+			opacity: 1,
+			duration: 0.6,
+			ease: "expo.inOut",
+		},
+		"-=7.9"
+	)
+	.to(
+		".animation-captions__item.caption--3",
+		{
+			opacity: 0,
+			duration: 0.4,
+			ease: "expo.inOut",
+		},
+		"-=7"
+	)
+	.to(
+		".animation-captions__item.caption--4",
+		{
+			opacity: 1,
+			duration: 0.6,
+			ease: "expo.inOut",
+		},
+		"-=6.9"
+	)
+	.to(
+		".animation-captions__item.caption--4",
+		{
+			opacity: 0,
+			duration: 0.4,
+			ease: "expo.inOut",
+		},
+		"-=6.2"
+	);
 
 function updateFrame(index) {
 	context.clearRect(0, 0, canvas.width, canvas.height);
@@ -56,13 +129,36 @@ function updateFrame(index) {
 
 function paintFrame(scrollData) {
 	let currentFrameIndex = Math.ceil(scrollData.progress * 100 * acceleration);
-	console.log(currentFrameIndex);
+	// console.log(scrollData.progress);
+	// if (scrollData.progress >= 0.7 && scrollData.progress <= 0.95) {
+	// 	if (!isAutoPlaying) {
+	// 		isAutoPlaying = true;
+	// 		autoScroll();
+	// 	}
+	// } else {
+	// 	if (isAutoPlaying) isAutoPlaying = false;
+	// 	if (currentFrameIndex < frameCount) {
+	// 		airpods.frame = currentFrameIndex;
+	// 	}
+	// }
 	if (currentFrameIndex < frameCount) {
 		airpods.frame = currentFrameIndex;
 	}
 }
 
+function autoScroll() {
+	let index = 1;
+	setInterval(() => {
+		if (index <= 135) {
+			airpods.frame = airpods.frame + 1;
+			console.log(airpods.frame, "current frame", index, "count");
+			index++;
+		}
+	}, 200);
+}
+
 function initCanvas() {
+	window.scrollTo(0, 0);
 	canvas.width = 1920;
 	canvas.height = 1080;
 	updateFrame(0);
@@ -71,6 +167,18 @@ function initCanvas() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+	const lenis = new Lenis();
+
+	lenis.on("scroll", (e) => {
+		console.log(e);
+	});
+
+	function raf(time) {
+		lenis.raf(time);
+		requestAnimationFrame(raf);
+	}
+
+	requestAnimationFrame(raf);
 	initCanvas();
 });
 
